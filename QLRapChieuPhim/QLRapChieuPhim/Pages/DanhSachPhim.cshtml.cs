62a72dbbb397f2beafa80a5672d325bbb5f4ce7b
ngocthuc2310 <ngocthuc2310@gmail.com>
2022-10-04 15:59:33 +0700

    4/10

--- QLRapChieuPhim/QLRapChieuPhim/Pages/DanhSachPhim.cshtml.cs
+++ QLRapChieuPhim/QLRapChieuPhim/Pages/DanhSachPhim.cshtml.cs
@@ -12,83 +12,54 @@ namespace QLRapChieuPhim.Pages
 {
     public class DanhSachPhimModel : PageModel
     {
-        public List<PhimModel> DanhSachPhim { get; private set; }
-        public List<TheLoaiPhimModel> DanhSachTheLoai { get; private set; }
-        public List<XepHangPhimModel> DanhSachXepHangPhim { get; private set; }
-
+        public List<Output.Types.Phim> DanhSachPhim { set; get; } = new List<Output.Types.Phim>();
+        public List<Output.Types.TheLoaiPhim> DanhSachTheLoai { set; get; } = new List<Output.Types.TheLoaiPhim>();
+        public Output.Types.TheLoaiPhim TheLoaiHienHanh { set; get; } = null;
+        [BindProperty(SupportsGet =true)]
+        public string Category { set; get; } = "";
         [BindProperty(SupportsGet = true)]
-        public int TheLoaiId { get; set; }
-
+        public int CurrentPage { set; get; } = 1;
+        public int PageCount { set; get; } = 1;
+        public int PageSize = 20;
         public void OnGet()
         {
             var channel = GrpcChannel.ForAddress(Common.ServiceLink);
             var client = new RapChieuPhim.RapChieuPhimClient(channel);
-
-            var responseTheloai = client.DanhSachTheLoai(new Input.Types.Empty());
-            var responseXephang = client.DanhSachXepHangPhim(new Input.Types.Empty());
-
-            if (TheLoaiId <= 0) TheLoaiId = responseTheloai.Items[0].Id;
-
-            var input = new Input.Types.PhimTheoTheLoai { TheLoaiId = this.TheLoaiId };
-            var response = client.DanhSachPhimTheoTheLoai(input);
-            DanhSachPhim = response.Items.Select(phim => new PhimModel
+            var idTheLoai = string.IsNullOrEmpty(Category) || Category == "all" ? 0 : int.Parse(Category);
+            Output.Types.Phims phims = client.DanhSachPhimTheoTheLoai(new Input.Types.PhimTheoTheLoai()
             {
-                Id = phim.Id,
-                TenPhim = phim.TenPhim,
-                TenGoc = phim.TenGoc,
-                DanhSachTheLoaiId = phim.DanhSachTheLoaiId,
-                DaoDien = phim.DaoDien,
-                DienVien = phim.DienVien,
-                NgayKhoiChieu = phim.NgayKhoiChieu.ToDateTime(),
-                NgonNgu = phim.NgonNgu,
-                NhaSanXuat = phim.NhaSanXuat,
-                NoiDung = phim.NoiDung,
-                NuocSanXuat = phim.NuocSanXuat,
-                Poster = phim.Poster,
-                ThoiLuong = phim.ThoiLuong,
-                Trailer = phim.Trailer,
-                XepHangPhimId = phim.XepHangPhimId
-            }).ToList();
-
-            
-            /*
-            foreach(var phim in DanhSachPhim)
+                TheLoaiId=idTheLoai,
+                CurrentPage=CurrentPage,
+                PageSize=PageSize
+            });
+            var TheLoais = client.DanhSachTheLoai(new Input.Types.Empty());
+            var XepHangPhims = client.DanhSachXepHangPhim(new Input.Types.Empty());
+            DanhSachTheLoai = TheLoais.Items.ToList();
+            if (!string.IsNullOrEmpty(Category) && Category != "all")
             {
-                var xephang = responseXephang.DanhSachXepHang.FirstOrDefault(x => x.Id.Equals(phim.XepHangPhimId));
-                if (xephang != null)
-                    phim.XepHangPhim = new XepHangPhimModel
-                    {
-                        Id = xephang.Id,
-                        KyHieu = xephang.KyHieu,
-                        Ten = xephang.Ten
-                    };
-
-                var dsTheloais = responseTheloai.DanhSachTheLoai
-                                                .Where(x => phim.DanhSachTheLoaiId.Contains("," + x.Id.ToString() + ","))
-                                                .Select(x => new TheLoaiPhimModel
-                                                {
-                                                    Id = x.Id,
-                                                    Ten = x.Ten
-                                                })
-                                                .ToList();
-                phim.DanhSachTheLoai = dsTheloais;
+                TheLoaiHienHanh = DanhSachTheLoai.FirstOrDefault(t => t.Id.Equals(int.Parse(Category)));
             }
-            */
-            DanhSachTheLoai = responseTheloai.Items
-                                            .Select(x => new TheLoaiPhimModel
-                                            {
-                                                Id = x.Id,
-                                                Ten = x.Ten
-                                            })
-                                            .ToList();
-            DanhSachXepHangPhim = responseXephang.Items
-                                                .Select(x => new XepHangPhimModel
-                                                {
-                                                    Id = x.Id,
-                                                    KyHieu = x.KyHieu,
-                                                    Ten = x.Ten
-                                                })
-                                                .ToList();
+            DanhSachPhim = phims.Items.ToList();
+            PageCount = phims.PageCount;
+            TheLoaiHienHanh = phims.TheLoaiHienHanh;
+            foreach (var phim in DanhSachPhim)
+            {
+                var dsIDTheLoai = phim.DanhSachTheLoaiId.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                var TheLoaiPhim = DanhSachTheLoai.Where(t => dsIDTheLoai.Contains(t.Id.ToString())).ToList();
+                if (TheLoaiPhim!=null)
+                {
+                    foreach (var tl in TheLoaiPhim)
+                    {
+                        phim.TheLoais.Add(new Output.Types.TheLoaiPhim
+                        {
+                            Id = tl.Id,
+                            Ten = tl.Ten
+                        });
+                    }
+                }
+                var Xephang = XepHangPhims.Items.FirstOrDefault(xh => xh.Id.Equals(phim.XepHangPhimId));
+                phim.XepHang = Xephang ?? new Output.Types.XepHangPhim();
+            }
         }
     }
 }
